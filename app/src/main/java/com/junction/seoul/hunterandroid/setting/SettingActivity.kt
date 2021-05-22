b3474bbfc7a6ad2c00ef96fbcefe4af366b13860
soo <onemask@naver.com>
2021-05-22 21:19:10 +0900

    SettingActivity 추가

---
+++ app/src/main/java/com/junction/seoul/hunterandroid/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.junction.seoul.hunterandroid.setting
+
+import com.junction.seoul.hunterandroid.R
+import com.junction.seoul.hunterandroid.base.BaseActivity
+import com.junction.seoul.hunterandroid.databinding.ActivitySettingBinding
+
+class SettingActivity : BaseActivity<ActivitySettingBinding>(layoutRes = R.layout.activity_setting) {
+
+  override fun onStart() {
+    super.onStart()
+  }
+}
