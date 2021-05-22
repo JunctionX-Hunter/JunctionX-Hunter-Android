566ca9ca8b4baf3429f64677b9d308ba8e054fc9
soo <onemask@naver.com>
2021-05-22 18:49:27 +0900

    목업 데이터 수정

--- app/src/main/java/com/junction/seoul/hunterandroid/data/BusNumber.kt
+++ app/src/main/java/com/junction/seoul/hunterandroid/data/BusNumber.kt
@@ -1,14 +1,16 @@
 package com.junction.seoul.hunterandroid.data
 
 data class BusNumber(
-  val busNumber: String
+  val title: String
 )
 
 //TODO 저장된 버스 숫자 가져오도록 수정 필요
 fun makeBusNumbers(): MutableList<BusNumber> {
   val list = mutableListOf<BusNumber>()
-  for (i in 0..5) {
-    list.add(BusNumber("100$i"))
-  }
+  list.add(BusNumber("6172"))
+  list.add(BusNumber("8172"))
+  list.add(BusNumber("60-2"))
+  list.add(BusNumber("1117"))
+  list.add(BusNumber("2000-1"))
   return list
 }
