e14360d2ff3e4d784f073a6a50bc8de2636dab6b
soo <onemask@naver.com>
2021-05-23 02:15:27 +0900

    세팅 연결

--- app/src/main/java/com/junction/seoul/hunterandroid/MainActivity.kt
+++ app/src/main/java/com/junction/seoul/hunterandroid/MainActivity.kt
@@ -2,5 +2,21 @@ package com.junction.seoul.hunterandroid
 
 import com.junction.seoul.hunterandroid.base.BaseActivity
 import com.junction.seoul.hunterandroid.databinding.ActivityMainBinding
+import com.junction.seoul.hunterandroid.setting.AddAndDeleteTextActivity
+import com.junction.seoul.hunterandroid.setting.SettingActivity
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+  override fun onStart() {
+    super.onStart()
+
+    binding.favorite.setOnClickListener {
+      AddAndDeleteTextActivity.launch(this)
+    }
+
+    binding.setting.setOnClickListener {
+      SettingActivity.launch(this)
+    }
+  }
+}
+
--- app/src/main/java/com/junction/seoul/hunterandroid/setting/SettingActivity.kt
+++ app/src/main/java/com/junction/seoul/hunterandroid/setting/SettingActivity.kt
@@ -1,12 +1,16 @@
 package com.junction.seoul.hunterandroid.setting
 
+import android.content.Context
+import android.content.Intent
 import com.junction.seoul.hunterandroid.R
 import com.junction.seoul.hunterandroid.base.BaseActivity
 import com.junction.seoul.hunterandroid.databinding.ActivitySettingBinding
 
 class SettingActivity : BaseActivity<ActivitySettingBinding>(layoutRes = R.layout.activity_setting) {
 
-  override fun onStart() {
-    super.onStart()
+  companion object {
+    fun launch(context: Context) {
+      context.startActivity(Intent(context, SettingActivity::class.java))
+    }
   }
 }
